e52ead5a9b6aba130cb56320a7be8efedfc5a81b
Sebastian <haschtl@online.de>
2019-03-11 13:53:35 +0100

    added sensor warnings

--- Futtertrocknung/Futtertrocknung.py
+++ Futtertrocknung/Futtertrocknung.py
@@ -28,18 +28,25 @@ class Plugin(LoggerPlugin):
         self.run = True
         self.samplerate = 1
 
+        self.aCCS_Error = False
+        self.bCCS_Error = False
+        self.aDHT_Error = False
+        self.bDHT_Error = False
+        self.cDHT_Error = False
+        self.dDHT_Error = False
+
         ccsat = Thread(target=self.getCCSAData)
         ccsat.start()
         ccsbt = Thread(target=self.getCCSBData)
         ccsbt.start()
 
-        dht22_a = Thread(target=self.getDHT22,args=(DHT_pins["A"],'aTemp', 'aHumid'))
+        dht22_a = Thread(target=self.getDHT22,args=(DHT_pins["A"],'aTemp', 'aHumid', self.aDHT_Error))
         dht22_a.start()
-        dht22_b = Thread(target=self.getDHT22,args=(DHT_pins["B"],'bTemp', 'bHumid'))
+        dht22_b = Thread(target=self.getDHT22,args=(DHT_pins["B"],'bTemp', 'bHumid', self.bDHT_Error))
         dht22_b.start()
-        dht22_c = Thread(target=self.getDHT22,args=(DHT_pins["C"],'cTemp', 'cHumid'))
+        dht22_c = Thread(target=self.getDHT22,args=(DHT_pins["C"],'cTemp', 'cHumid', self.cDHT_Error))
         dht22_c.start()
-        dht22_d = Thread(target=self.getDHT22,args=(DHT_pins["D"],'dTemp', 'dHumid'))
+        dht22_d = Thread(target=self.getDHT22,args=(DHT_pins["D"],'dTemp', 'dHumid', self.dDHT_Error))
         dht22_d.start()
 
     def getCCSAData(self):
@@ -55,8 +62,15 @@ class Plugin(LoggerPlugin):
                 self.stream([ccs1.eco2,ccs1.tvoc],  ['aCO2', 'aTVOC'], dname=devicename, unit = ['ppm','ppm'])
                 if self.data[0]>2000:
                     print('event')
-                    self.event('CO2 Gehalt hoch', sname="CO2", dname="Futtertrocknung", priority=2)
+                    self.event('CO2 Gehalt hoch', sname="aCO2", dname="Futtertrocknung", priority=1)
+                if self.aCCS_Error:
+                    self.aCCS_Error = False
+                    self.event('CCS811 A: Sensorfehler wurde behoben', sname="aCO2", dname="Futtertrocknung", priority=2)
+                    print("CCS811 A Error fixed")
             except:
+                if not self.aCCS_Error:
+                    self.aCCS_Error = True
+                    self.event('CCS811 A: Sensorfehler!', sname="aCO2", dname="Futtertrocknung", priority=1)
                 print("Error reading CCS811 A")
 
     def getCCSBData(self):
@@ -70,14 +84,30 @@ class Plugin(LoggerPlugin):
             time.sleep(1/self.samplerate)
             try:
                 self.stream([ccs2.eco2,ccs2.tvoc],  ['bCO2', 'bTVOC'], dname=devicename, unit = ['ppm','ppm'])
+                if self.bCCS_Error:
+                    self.bCCS_Error = False
+                    self.event('CCS811 B: Sensorfehler wurde behoben', sname="bCO2", dname="Futtertrocknung", priority=2)
+                    print("CCS811 B Error fixed")
             except:
+                if not self.bCCS_Error:
+                    self.bCCS_Error = True
+                    self.event('CCS811 B: Sensorfehler!', sname="bCO2", dname="Futtertrocknung", priority=1)
                 print("Error reading CCS811 B")
 
-    def getDHT22(self, pin, tName, hName):
+    def getDHT22(self, pin, tName, hName, error):
         while self.run:
             time.sleep(1/self.samplerate)
             humidity, temperature = Adafruit_DHT.read_retry(dht22, pin)
-            self.stream([temperature,humidity],  [tName, hName], dname=devicename, unit = ['°C','%'])
+            if humidity != None and temperature != None:
+                self.stream([temperature,humidity],  [tName, hName], dname=devicename, unit = ['°C','%'])
+                if error:
+                    error = False
+                    self.event('DHT22 '+tName+': Sensorfehler wurde behoben', sname=tName, dname="Futtertrocknung", priority=2)
+            else:
+                if not error:
+                    error = True
+                    self.event('DHT22 '+tName+': Sensorfehler!', sname=tName, dname="Futtertrocknung", priority=1)
+                print('Cannot read DHT22, Pin '+str(pin))
 
     def getControllerData(self):
         self.data['fanVelocity'] = '?'
