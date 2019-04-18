65461d175225c7190e659d1db6e1d9061cea4522
Sebastian <haschtl@online.de>
2019-04-18 21:12:56 +0200

    asf

--- Futtertrocknung_Controller/Futtertrocknung_Controller.py
+++ Futtertrocknung_Controller/Futtertrocknung_Controller.py
@@ -44,8 +44,13 @@ class Plugin(LoggerPlugin, controller):
             start_time = time.time()
 
             # Stream all measurements
+            rpm = self.rpm
+            pressure = self.air_pressure
+            temp1 = self.temperature1
+            temp2 = self.temperature2
+            flow = self.flow_rate
             sensor_data = {
-                'E': {'Drehzahl': [self.rpm, 'U/min'], 'Luftdruck': [self.air_pressure, 'bar'], 'Temperatur1': [self.temperature1, '°C'], 'Temperatur2': [self.temperature2, '°C'], 'Durchfluss': [self.flow_rate, 'm³/s']}
+                'E': {'Drehzahl': [rpm, 'U/min'], 'Luftdruck': [pressure, 'bar'], 'Temperatur1': [temp1, '°C'], 'Temperatur2': [temp2, '°C'], 'Durchfluss': [flow, 'm³/s']}
             }
             #print(sensor_data)
             self.stream(list=sensor_data)
