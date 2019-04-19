c6c08e07b7700d9359fc9aeda4f6172deced4500
Sebastian <haschtl@online.de>
2019-04-19 03:05:57 +0200

    sdh

--- Futtertrocknung_Sensoren/Futtertrocknung_Sensoren.py
+++ Futtertrocknung_Sensoren/Futtertrocknung_Sensoren.py
@@ -98,6 +98,14 @@ class Plugin(LoggerPlugin):
         self._thread = Thread(target=self._sensorThread)
         self._thread.start()
 
+        self._sensor_data = {
+            'A': {'Temperatur': [0, '°C'], 'CO2-Gehalt': [0, 'ppm'], 'TVOC-Gehalt': [0, 'ppm'], 'Temperatur2': [0, '°C'], 'Feuchtigkeit': [0, '%']},
+            'B': {'Temperatur': [0, '°C'], 'CO2-Gehalt': [0, 'ppm'], 'TVOC-Gehalt': [0, 'ppm'], 'Temperatur': [0, '°C'], 'Feuchtigkeit': [0, '%']},
+            'C': {'Temperatur': [0, '°C'], 'Feuchtigkeit': [0, '%']},
+            'D': {'Temperatur': [0, '°C'], 'Feuchtigkeit': [0, '%']},
+            'Bedienelement': {'CPU-Temperatur': [0, '°C']},
+        }
+
     def _waitForSensors(self):
         # Wait for the sensor to be ready and calibrate the thermistor
         if ccs1 is not None:
@@ -259,8 +267,8 @@ class Plugin(LoggerPlugin):
                 tvoc_a = self._processSensor('A', 'CCS', 'TVOC-Gehalt', tvoc_a)
                 self._sensorErrorEvent('A', 'CCS', False)
         except:
-            co2_a = 0
-            tvoc_a = 0
+            co2_a = self.sensor_data["A"]['CO2-Gehalt'][0]
+            tvoc_a = self.sensor_data['A']['TVOC-Gehalt'][0]
             #self._sensorErrorEvent('A', 'CCS', True)
             #print(traceback.format_exc())
         try:
@@ -274,12 +282,12 @@ class Plugin(LoggerPlugin):
                 tvoc_b = self._processSensor('B', 'CCS', 'TVOC-Gehalt', tvoc_b)
                 self._sensorErrorEvent('B', 'CCS', False)
         except:
-            co2_b = 0
-            tvoc_b = 0
+            co2_b = self.sensor_data["B"]['CO2-Gehalt'][0]
+            tvoc_b = self.sensor_data['B']['TVOC-Gehalt'][0]
             #self._sensorErrorEvent('B', 'CCS', True)
             #print(traceback.format_exc())
 
-        sensor_data = {
+        self.sensor_data = {
             'A': {'Temperatur': [aTemp, '°C'], 'CO2-Gehalt': [co2_a, 'ppm'], 'TVOC-Gehalt': [tvoc_a, 'ppm'], 'Temperatur2': [aTemp, '°C'], 'Feuchtigkeit': [aHumid, '%']},
             'B': {'Temperatur': [bTemp, '°C'], 'CO2-Gehalt': [co2_b, 'ppm'], 'TVOC-Gehalt': [tvoc_b, 'ppm'], 'Temperatur': [bTemp, '°C'], 'Feuchtigkeit': [bHumid, '%']},
             'C': {'Temperatur': [cTemp, '°C'], 'Feuchtigkeit': [cHumid, '%']},
@@ -287,7 +295,7 @@ class Plugin(LoggerPlugin):
             'Bedienelement': {'CPU-Temperatur': [rpiTemp, '°C']},
         }
 
-        return sensor_data
+        return self.sensor_data
 
     def trySensorRead(self, value1, messtelle, sensor, signal, signal2, processed=True, gain=1):
         try:
@@ -301,8 +309,8 @@ class Plugin(LoggerPlugin):
                 b = self._processSensor(messtelle, sensor, signal2, b)
             self._sensorErrorEvent(messtelle, sensor, False)
         except:
-            a = 0
-            b = 0
+            a = self.sensor_data[messtelle][signal][0]
+            b = self.sensor_data[messtelle][signal2][0]
             #self._sensorErrorEvent(messtelle, sensor, True)
             #print(traceback.format_exc())
         return a,b
