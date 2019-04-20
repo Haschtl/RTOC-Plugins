eece18a433acb1fc816dacefb0371d9271f56c62
Sebastian <haschtl@online.de>
2019-04-20 07:48:12 +0200

    sd

--- Futtertrocknung_Sensoren/ccs811.py
+++ Futtertrocknung_Sensoren/ccs811.py
@@ -93,18 +93,6 @@ class CCS811:
     #print(i2c_bit.ROBit(0xe0, 4))
     #print(i2c_bit.ROBit(0xe0, 5))
 
-    """True when an error has occured."""
-    data_ready = i2c_bit.ROBit(0x00, 3)
-    """True when new data has been read."""
-    app_valid = i2c_bit.ROBit(0x00, 4)
-    fw_mode = i2c_bit.ROBit(0x00, 7)
-
-    hw_id = i2c_bits.ROBits(8, 0x20, 0)
-
-    int_thresh = i2c_bit.RWBit(0x01, 2)
-    interrupt_enabled = i2c_bit.RWBit(0x01, 3)
-    drive_mode = i2c_bits.RWBits(3, 0x01, 4)
-
     temp_offset = 0.0
     """Temperature offset."""
 
@@ -135,6 +123,17 @@ class CCS811:
             i2c.write(buf, end=1, stop=True)
         time.sleep(.1)
 
+        """True when an error has occured."""
+        self.data_ready = i2c_bit.ROBit(0x00, 3)
+        """True when new data has been read."""
+        self.app_valid = i2c_bit.ROBit(0x00, 4)
+        self.fw_mode = i2c_bit.ROBit(0x00, 7)
+
+        self.hw_id = i2c_bits.ROBits(8, 0x20, 0)
+
+        self.int_thresh = i2c_bit.RWBit(0x01, 2)
+        self.interrupt_enabled = i2c_bit.RWBit(0x01, 3)
+        self.drive_mode = i2c_bits.RWBits(3, 0x01, 4)
         #make sure there are no errors and we have entered application mode
         #if self.error:
         #    raise RuntimeError("Device returned a error! Try removing and reapplying power to "
