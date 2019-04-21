616dbe00eef776a8f6b0c7054535de9d0406e3f4
Sebastian <haschtl@online.de>
2019-04-21 18:14:28 +0200

    asd

--- Futtertrocknung_Sensoren/bib/dht22.py
+++ Futtertrocknung_Sensoren/bib/dht22.py
@@ -265,7 +265,6 @@ class DHTBase:
             data returned from the device (try again)
         """
         self.measure()
-        print(self._temperature)
         self._temperature=self.temperature_filter.statistic_filter(self._temperature)
         self._humidity=self.humidity_filter.statistic_filter(self._temperature)
         return self._temperature,self._humidity
