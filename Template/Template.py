716a3d594bbf8297a590bc18627dbcf0b7472784
Sebastian <haschtl@online.de>
2019-06-02 23:13:00 +0200

    sdf

--- Template/Template.py
+++ Template/Template.py
@@ -1,7 +1,10 @@
-# This template shows, how to implement plugins in RTOC
-# RTOC version 2.0
+"""
+This template shows, how to implement plugins in RTOC
 
-# A plugin needs to import RTOC.LoggerPlugin to be recognized by RTOC.
+RTOC version 2.0
+
+A plugin needs to import RTOC.LoggerPlugin to be recognized by RTOC.
+"""
 try:
     from LoggerPlugin import LoggerPlugin
 except ImportError:
@@ -13,69 +16,101 @@ from PyQt5 import uic
 from PyQt5 import QtWidgets
 import numpy as np
 
-DEVICENAME = "Template"  # Define the name of this Device
+DEVICENAME = "Template"
+"""Definition of the devicename outside of plugin-class"""
 
-AUTORUN = True  # If true, the thread to collect data will run right after initializing this plugin
-SAMPLERATE = 1  # The thread,which is supposed to collect data will be executed with 1 Hz
+AUTORUN = True
+"""If true, the thread to collect data will run right after initializing this plugin"""
+SAMPLERATE = 1
+"""The thread,which is supposed to collect data will be executed with 1 Hz"""
 
 
 class Plugin(LoggerPlugin):
     def __init__(self, stream=None, plot=None, event=None):
-        # Plugin configuration
-        # Call this to initialize RTOC.LoggerPlugin
         super(Plugin, self).__init__(stream, plot, event)
-        # Set a default devicename.
-        # This will be used for all signals sent by this plugin as default
-        self.setDeviceName(DEVICENAME)
+        """Call this to initialize RTOC.LoggerPlugin"""
 
-        # GUI configuration
-        self.smallGUI = True  # Only cares, if your Plugin has a GUI
+        self.setDeviceName(DEVICENAME)
+        """
+        Set a default devicename.
+        This will be used for all signals sent by this plugin as default
+        """
+
+        self.smallGUI = True
+        """This plugin has a GUI, which is small. GUI can be shown in two different ways."""
 
         self._firstrun = True
 
-        # set the function, which will collect data periodically
         self.setPerpetualTimer(self._updateT, samplerate=SAMPLERATE)
+        """You will need to collect data periodically in many applications. You need to start that in a seperate thread.
+
+        RTOC provides a simple way to start a repeated thread with :py:meth:`.LoggerPlugin.setPerpetualTimer`. The first parameter is the function, which collects data and sends it to RTOC. You can define a ``samplerate`` or an ``interval`` to set the samplerate.
+
+        You can still use normal threads to do the same thing, but in this way, the plugin can be stopped properly. If you are using normal threads, make sure, to have a loop limited by '
+        ``self.run`` with ``while self.run:``.
+        """
+
         if AUTORUN:
-            # start the configured perpetual timer. It can be stopped with 'self.cancel()'
             self.start()
+            """
+            Start the configured perpetualTimer. You can stop it with ``self.cancel()``. If you want to start data collection, when this plugin is started, you need to call ``self.start()`` in the plugin-initialization.
+            """
 
-        # Instead of this, you can also start a normal thread, which is active in this while loop: 'while self.run:'. This ensures, that this plugin can be stopped.
-
-    # This function is being called in a thread.
     def _updateT(self):
-        # Do something, collect data ,...
-        # for example:
+        """
+        This function is called periodically after calling ``self.start()``.
+
+        This example will generate a sinus and a cosinus curve. And send them to RTOC.
+
+
+        """
         y1 = np.sin(time.time())
         y2 = np.cos(time.time())
 
-        # Then send your data
-        self.stream([y1, y2], snames=['Sinus', 'Cosinus'],
-                    unit=["kg", "m"])  # send data to RTOC
+        self.stream([y1, y2], snames=['Sinus', 'Cosinus'], unit=["kg", "m"])
+        """
+        Use this function to send data to RTOC: :py:meth:`.LoggerPlugin.stream`
+        """
 
-        # You can also plot data like this:
         self.plot([-10, 0], [2, 1], sname='Plot', unit='Wow')
-
-        # Or send an event with self.event(text='',sname='')
-        # (but use with caution, it can spam your RTOC plots):
+        """
+        Use this function to send data to RTOC: :py:meth:`.LoggerPlugin.plot`
+        """
         if self._firstrun:
             self.event('Test event', sname='Plot', id='testID')
+            """
+            Use this function to send an event to RTOC: :py:meth:`.LoggerPlugin.event`
+            """
             self._firstrun = False
 
-    # This function is used to initialize a own Plugin-GUI,
-    # which will be available in RTOC.
-    # Remove it, if you don't want a GUI for your plugin.
     def loadGUI(self):
-        self.widget = QtWidgets.QWidget()  # Create an empty QWidget
-        packagedir = self.getDir(__file__)  # Get filepath of this file
-        # Load a GUI designed with QDesigner
+        """
+        This function is used to initialize the Plugin-GUI, which will be available in :doc:`GUI`.
+
+        This is optional.
+
+        Returns:
+            PyQt5.QWidget: A widget containing optional plugin-GUI
+        """
+        self.widget = QtWidgets.QWidget()
+        """
+        Create an empty QWidget
+        """
+        packagedir = self.getDir(__file__)
+        """Get filepath of this file"""
         uic.loadUi(packagedir+"/Template/template.ui", self.widget)
+        """
+        This example will load a QWidget designed with QDesigner
+        """
         return self.widget  # This function needs to return a QWidget
 
 
-# Sometimes you want to use plugins standalone also. Very useful for testing.
 hasGUI = True  # If your plugin has a widget do this
 
 if __name__ == "__main__":
+    """
+    Sometimes you want to use plugins standalone also. This is very useful for testing.
+    """
     if hasGUI:
         app = QtWidgets.QApplication(sys.argv)
         myapp = QtWidgets.QMainWindow()
